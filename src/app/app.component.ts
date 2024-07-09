c31e6afcf63805b69cd628c99d05787f5732e354
Shehroze Islam <shehroze@mislbd.com>
2024-07-09 12:45:45 +0600

    Implemented property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,26 @@ export class AppComponent {
   selector: 'app-root',
   template: `
   <ol>
-    <li *ngFor="let name of names">{{ name }}</li>
+    <li *ngFor="let person of people">
+      <a [hidden]="!person.hidden" (click)="toggle(person)" style="color: blue">Show</a>
+      <span [hidden]="person.hidden" (click)="toggle(person)">{{ person.name }}</span></li>
   </ol>
   `
 })
 export class LoopComponent {
-  names: String[];
+  people: Object[];
 
   constructor() {
-    this.names = [
-      "Osman",
-      "Bodrul",
-      "Rizvi",
-      "Nayeem",
-      "Rabiul"
+    this.people = [
+      {"name": "Osman", "hidden": true},
+      {"name": "Bodrul", "hidden": true},
+      {"name": "Rizvi", "hidden": true},
+      {"name": "Nayeem", "hidden": true},
+      {"name": "Rabiul", "hidden": true}
     ]
+  }
+
+  toggle(person) {
+    person.hidden = !person.hidden;
   }
 }
